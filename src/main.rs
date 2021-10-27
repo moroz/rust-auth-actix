2f60474c172a78e7188082e7115ac7b4f3b90d82
Karol Moroz <k.j.moroz@gmail.com>
2021-10-28 00:01:02 +0200

    Stub new modules

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
 #[macro_use]
 extern crate actix_web;
 
+#[macro_use]
+extern crate diesel;
+
+use actix_web::dev::ServiceRequest;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use diesel::prelude::*;
+use diesel::r2d2::{self, ConnectionManager};
 
+mod errors;
 mod handlers;
+mod models;
+mod schema;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
+pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
             .route("/users", web::get().to(handlers::get_users))
             .route("/users/{id}", web::get().to(handlers::get_user_by_id))
             .route("/users", web::post().to(handlers::add_user))
