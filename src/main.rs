85585fa09705eb439c133a87f0025363952352f8
Karol Moroz <k.j.moroz@gmail.com>
2021-10-27 23:53:21 +0200

    Stub CRUD routes

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ extern crate actix_web;
 
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
+mod handlers;
+
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -10,8 +12,15 @@ async fn hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .route("/users", web::get().to(handlers::get_users))
+            .route("/users/{id}", web::get().to(handlers::get_user_by_id))
+            .route("/users", web::post().to(handlers::add_user))
+            .route("/users/{id}", web::delete().to(handlers::delete_user))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
