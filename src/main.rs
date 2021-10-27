d023741281ce01406b3a5de666fbf89cb20d4c01
Karol Moroz <k.j.moroz@gmail.com>
2021-10-28 00:12:48 +0200

    Setup database

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 #[macro_use]
-extern crate actix_web;
-
-#[macro_use]
 extern crate diesel;
 
 use actix_web::dev::ServiceRequest;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager};
+use std::env;
 
 mod errors;
 mod handlers;
@@ -18,8 +16,17 @@ pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    env::set_var("RUST_LOG", "actix_web=debug");
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL not set");
+
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool: Pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.");
+
+    HttpServer::new(move || {
         App::new()
+            .data(pool.clone())
             .route("/users", web::get().to(handlers::get_users))
             .route("/users/{id}", web::get().to(handlers::get_user_by_id))
             .route("/users", web::post().to(handlers::add_user))
